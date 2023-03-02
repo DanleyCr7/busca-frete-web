5a11a8e03ba641a3a42cc56eb4e4ea9c0297a0ac
Danrley Silva <danrleysil46@gmail.com>
2023-03-02 08:49:41 -0300

    center div

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,8 +38,8 @@ export default function Home() {
             <p className="text-xs md:text-lg lg:text-lg">Agora viva em Parnaíba! A partir de hoje, o busca frete está chegando.</p>
           </div>
           {/* <--> */}
-          <div className="w-6/6 text-center">
-            <Image className="mt-8" src={busca_frete} alt="busca frete" />
+          <div className="text-center">
+            <Image className="mt-8 w-2/6" src={busca_frete} alt="busca frete" />
             {/* <h1 className="text-2xl lg:text-4xl font-light">Com o apertar de um botão</h1> */}
           </div>
           
