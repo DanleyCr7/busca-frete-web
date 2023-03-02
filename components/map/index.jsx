38b0d5881cb8c519118e201c288ed83a3c1dfbe2
Danrley Silva <danrleysil46@gmail.com>
2023-03-02 00:04:33 -0300

    remove trash

--- components/map/index.jsx
+++ components/map/index.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import Head from 'next/head';
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -2.90453,
-  lng: -41.7523
-};
 
 
 function MyMap() {
-  const { styles } = this.props;
 
   return (
     <Html lang="en">
